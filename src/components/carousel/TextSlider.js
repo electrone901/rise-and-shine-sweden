de7fee92a373b9f9857619c26d3459c2cb35f6ce
You Song Hou <yousonghou@yahoo.com>
2020-04-05 15:21:15 -0400

    fix the text slider title

--- src/components/carousel/TextSlider.js
+++ src/components/carousel/TextSlider.js
@@ -19,14 +19,12 @@ class TextSlider extends Component {
       <div className="tParent">
         <div className="row">
           <div className="col text-center">
-            <h1 className="textSliderTitle">Discover events for all the</h1>
-            <h2 className="textSliderTitle">things you love </h2>
-            <p className="tParent__items__subTittle">We do more together</p>
-            {this.state.auth ? null : (
-              <Link to="/register" className="tParent__items__sign">
-                Join now
-              </Link>
-            )}
+            <h1 className="textSliderTitle bd-highlight">Discover events for all the</h1>
+            <h2 className="textSliderTitle bd-highlight">things you love </h2>
+            <p className="tParent__items__subTittle text-dark">We do more together</p>
+            <a href="https://www.facebook.com/riseandshinesweden/" target="blank" className="btn btn-lg btn-primary">
+              Join now
+            </a>
           </div>
         </div>
       </div>
